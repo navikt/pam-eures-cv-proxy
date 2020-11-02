a052aed993daeb95472a248ea5ba1267a098fcb2
Einar Bjerve <einar.bjerve@nav.no>
2020-11-02 12:53:37 +0100

    Reactive controller

--- src/main/kotlin/no/nav/cv/eures/proxy/ProxyController.kt
+++ src/main/kotlin/no/nav/cv/eures/proxy/ProxyController.kt
@@ -7,12 +7,12 @@ import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
-import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
 
 @Controller("input/api/cv/v1.0")
 class ProxyController (
-        @Client("\${pam-eures-cv-eksport.url}")  private val client: HttpClient,
+        @Client("\${pam-eures-cv-eksport.url}")  private val client: RxHttpClient,
         @Value("\${pam-eures-cv-eksport.scheme}") private val scheme: String,
         @Value("\${pam-eures-cv-eksport.proxiedhost}") private val host: String,
         @Value("\${pam-eures-cv-eksport.proxiedport}") private val port: Int
@@ -20,19 +20,19 @@ class ProxyController (
 
 
         @Get("ping")
-        fun ping() = client.toBlocking().retrieve("/input/api/cv/v1.0/ping")
+        fun ping() = client.retrieve("/input/api/cv/v1.0/ping")
 
         @Get("getAll", produces = ["application/json"])
         fun getAll() =
-                client.toBlocking().retrieve("/input/api/cv/v1.0/getAll")
+                client.retrieve("/input/api/cv/v1.0/getAll")
 
         @Get("getChanges/{modificationTimestamp}", produces = ["application/json"])
         fun getChanges(modificationTimestamp: String) =
-                client.toBlocking().retrieve("/input/api/cv/v1.0/getChanges/$modificationTimestamp")
+                client.retrieve("/input/api/cv/v1.0/getChanges/$modificationTimestamp")
 
         @Post("getDetails", produces = ["application/json"])
         fun getDetails(@Body references: String) =
-                client.toBlocking().exchange(
+                client.exchange(
                         HttpRequest
                                 .POST("/input/api/cv/v1.0/ping", references)
                                 .contentType(MediaType.APPLICATION_JSON_TYPE)
