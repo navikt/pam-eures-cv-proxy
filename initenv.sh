631090e7a0e2e8c6709b78487b41c51417051146
Julian Torp <julian.torp@nav.no>
2022-02-01 10:39:32 +0100

    simplify nginx.conf

--- initenv.sh
+++ initenv.sh
@@ -3,6 +3,5 @@ set -e
 for name in log client_temp proxy_temp_path fastcgi_temp uwsgi_temp scgi_temp; do
     mkdir -vp /tmp/nginx/$name
 done
-ln -s -f /dev/stdout /tmp/nginx/log/access.log
-ln -s -f /dev/stderr /tmp/nginx/log/error.log
+
 chown -R nginx:nginx /tmp/nginx
